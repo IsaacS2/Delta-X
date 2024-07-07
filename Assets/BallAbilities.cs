6403ec0368cc5596d76ba05b620baf707b5fa9d3
IsaacS2Hud <sanchezisaac809@gmail.com>
2024-07-06 21:15:48 -0500

    Adds player control
    Adds new input system to allow for customizable controls for different inputs, and applies movement controls and explosion input recognition to temporary player object.

---
+++ Assets/BallAbilities.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class BallAbilities : MonoBehaviour
+{
+    public Rigidbody2D _rb;
+
+    [SerializeField] private float _moveSpeed = 3;
+
+    private Vector2 _moveDirection;
+
+    public InputActionReference move;
+    public InputActionReference fire;
+
+    // Update is called once per frame
+    void Update()
+    {
+        _moveDirection = move.action.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        _rb.velocity = new Vector2(_moveDirection.x * _moveSpeed, 
+            _moveDirection.y * _moveSpeed);
+        Debug.Log(_rb.velocity);
+    }
+
+    private void OnEnable()
+    {
+        fire.action.started += Fire;
+    }
+
+    private void OnDisable()
+    {
+        fire.action.started -= Fire;
+    }
+
+    private void Fire(InputAction.CallbackContext obj)
+    {
+        Debug.Log("Fired");
+    }
+}
